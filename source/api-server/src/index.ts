22331d91d3645ba741af038d62153baec119cc77
arnekristian58 <arnekristiannetland@gmail.com>
2023-02-28 08:07:52 +0100

    tester+api

--- source/api-server/src/index.ts
+++ source/api-server/src/index.ts
@@ -1,13 +1,48 @@
 import express from 'express';
-import axios from 'axios';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({
-      message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-    });
-  });
+
+app.get('/customers', (req, res) => {
+    console.log('received customers');
+    res.json({ message: 'received customers',});
+});
+app.get('/customers/:NO', (req, res) => {
+  console.log(`received customers: ${req.params.NO}`);
+  res.json({ message: `received customers: ${req.params.NO}`,});
+});
+app.get('/salesorders', (req, res) => {
+  console.log('received salesorders');
+  res.json({ message: 'received salesorders',});
+});
+app.get('/salesorders/:NO', (req, res) => {
+  console.log(`received salesorders ${req.params.NO}`);
+  res.json({ message: `received salesorders: ${req.params.NO}`,});
+});
+app.get('/productionorders', (req, res) => {
+  console.log('received productionorders');
+  res.json({ message: 'received productionorders',});
+});
+app.get('/productionorders/:NO', (req, res) => {
+  console.log(`received productionorders: ${req.params.NO}`);
+  res.json({ message: `received productionorders: ${req.params.NO}`,});
+});
+app.get('/invoices', (req, res) => {
+  console.log('received invoices');
+  res.json({ message: 'received invoices',});
+});
+app.get('/invoices/:NO', (req, res) => {
+  console.log(`received invoices ${req.params.NO}`);
+  res.json({ message: `received invoices: ${req.params.NO}`,});
+});
+app.get('/payments', (req, res) => {
+  console.log('received payments');
+  res.json({ message: 'received payments',});
+});
+  app.get('/payments/:NO', (req, res) => {
+  console.log(`received payments ${req.params.NO}`);
+  res.json({ message: `received payments: ${req.params.NO}`,});
+});
 
 app.listen(4000, () => {
   console.log(`server running on port 4000`);
--- source/tester/src/index.ts
+++ source/tester/src/index.ts
@@ -1,10 +1,67 @@
 import axios from 'axios';
 
-async function getUser() {
-    try {
-      const response = await axios.get('/user?ID=12345');
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+console.log('Tester started..')
+
+
+function getApi(): string {
+
+  const endpoints = [
+    'customers',
+    'customers/<NO>',
+    'salesorders',
+    'salesorders/<NO>',
+    'productionOrders',
+    'productionOrders/<NO>',
+    'invoices',
+    'invoices/<NO>',
+    'payments',
+    'payments/<NO>'
+  ]
+
+  const index = Math.floor(Math.random() * 10);
+
+  console.log(`index: ${index}`)
+
+  let ret = endpoints[index].replace('<NO>', (Math.round(Math.random() * 900000) + 100000).toString());
+
+  return ret;
+}
+
+
+
+const makeRequest = async () => {
+
+  console.log('calling api')
+
+  const endpoint = getApi();
+
+  console.log(`http://api-server:4000/${endpoint}`);
+
+  try {
+    console.log('Calling api');
+    const response = await axios.get(`http://api-server:4000/${endpoint}`);
+    
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }    
+
+  // Sometime in the next two seconds, but larger than 100ms
+  const nextReqIn = (Math.random() * 1000) + 100;
+  setTimeout(() => makeRequest(), nextReqIn); 
+}
+    
+   
+
+
+
+(async () => {
+
+  // Kick off four requests that cycle at regular intervals
+  setTimeout(() => makeRequest(), 5000);
+  //setTimeout(() => makeRequest(), 6000);
+  //setTimeout(() => makeRequest(), 7000);
+  //setTimeout(() => makeRequest(), 8000);
+
+ 
+})();
